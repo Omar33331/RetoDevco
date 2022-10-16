dfb21fdb9778d01f492e2b7df7b80ebdb7143457
Omar Murcia <omar3331@gmail.com>
2022-10-16 00:41:49 -0500

    estructura del primer escenario

---
+++ RetoDevcoAuto/src/test/java/co/com/booking/stepdefinitions/HostingSearchStepDefinitions.java
@@ -0,0 +1,22 @@
+package co.com.booking.stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class HostingSearchStepDefinitions {
+    @Given("^the user is on the booking page$")
+    public void theUserIsOnTheBookingPage() {
+
+    }
+
+    @When("^he enters the search data$")
+    public void heEntersTheSearchData() {
+
+    }
+
+    @Then("^he looks at the hosting options$")
+    public void heLooksAtTheHostingOptions() {
+
+    }
+}
---
+++ RetoDevcoAuto/src/test/resources/features/hosting_search.feature
@@ -0,0 +1,9 @@
+#languaje:en
+
+Feature: Make a search of hosting in booking page
+
+  @Hosting1
+  Scenario: tage outline: Make a search of hosting in booking page
+    Given the user is on the booking page
+    When he enters the search data
+    Then he looks at the hosting options
